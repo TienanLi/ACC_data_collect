794b0da13844f36143269d3ec5e4a04e06a9a2e0
TienanLi <ironmantnt@live.com>
2020-02-17 12:04:00 -0500

    simplify the usage for command line

--- GPS_Logger.py
+++ GPS_Logger.py
@@ -1,35 +1,43 @@
 import serial
 import time
 import csv
+import sys
 from RecordGPS import UbxPacketizer,decode_packet,SolutionPacket
 
 
-if __name__ == "__main__":
-    GPS_ser = serial.Serial(port='/dev/ttyACM0')
-    start_time=time.strftime("%d%H%M%S", time.localtime())
-    print(start_time)
+def write_GPS(GPS_ser,csvwriter,packetizer):
+    l = GPS_ser.read(1)
+    packet = packetizer.add_byte(l[0])
+    if packet is not None:
+        decoded_packet = decode_packet(packet)
+        if type(decoded_packet) == SolutionPacket:
+            t = time.time()
+            # print(t)
+            my_packet = decoded_packet
+            csvwriter.writerow([t,  # time from record start
+                                my_packet.time_of_week,  # GPS time
+                                my_packet.llh_position[0],  # lat
+                                my_packet.llh_position[1],  # lon
+                                my_packet.llh_position[2],  # height
+                                my_packet.ground_speed  # GPS_speed(m/s)
+                                ])
+
+def main(port_name):
+    GPS_ser = serial.Serial(port=port_name)
+    start_time = time.strftime("%d%H%M%S", time.localtime())
     outputfile = open('output' + start_time + '.csv', 'w')
+    print("Writing GPS csv file...\n" + start_time)
     csvwriter = csv.writer(outputfile)
-    print("Writing GPS csv file...\n")
     packetizer = UbxPacketizer()
     while True:
         try:
-            l = GPS_ser.read(1)
-            packet = packetizer.add_byte(l[0])
-            if packet is not None:
-                decoded_packet = decode_packet(packet)
-                if type(decoded_packet) == SolutionPacket:
-                    t = time.time()
-                    # print(t)
-                    my_packet = decoded_packet
-                    csvwriter.writerow([t,#time from record start
-                                        my_packet.time_of_week,#GPS time
-                                        my_packet.llh_position[0],#lat
-                                        my_packet.llh_position[1],#lon
-                                        my_packet.llh_position[2],#height
-                                        my_packet.ground_speed#GPS_speed(m/s)
-                                        ])
-
+            write_GPS(GPS_ser, csvwriter, packetizer)
         except KeyboardInterrupt:
             GPS_ser.close()
             outputfile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+    # main('COM3')
+
--- OBDII_logger_TL.py
+++ OBDII_logger_TL.py
@@ -1,10 +1,10 @@
 import time
 import csv
 import obd
+import sys
 
-
-if __name__ == "__main__":
-    connection=obd.OBD('/dev/rfcomm0')
+def main(port_name,MAF=True):
+    connection=obd.OBD(port_name)
     start_time=time.strftime("%d%H%M%S", time.localtime())
     print(start_time)
     outputfile = open('output' + start_time + '.csv', 'w')
@@ -14,11 +14,19 @@ if __name__ == "__main__":
         try:
             t = time.time()
             OBD_speed = connection.query(obd.commands.SPEED).value.magnitude
-            OBD_max_air_flow = connection.query(obd.commands.MAF).value.magnitude
-            csvwriter.writerow([t,#time from record start
-                                OBD_max_air_flow,#gram/sec
-                                OBD_speed#m/s
-                                ])
-
+            if MAF:
+                OBD_max_air_flow = connection.query(obd.commands.MAF).value.magnitude
+                csvwriter.writerow([t,#time from record start
+                                    OBD_max_air_flow,#gram/sec
+                                    OBD_speed#m/s
+                                    ])
+            else:
+                csvwriter.writerow([t,  # time from record start
+                                    OBD_speed  # m/s
+                                    ])
         except KeyboardInterrupt:
             outputfile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2])
